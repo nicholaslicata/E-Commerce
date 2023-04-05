c5408f933853604f37ca819a3a45daede6f194e5
Nicholas Licata <nicholas.licata730@gmail.com>
2023-04-05 00:03:02 -0400

    set up routing for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Store from './pages/Store';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='store' element={<Store />} />
+          <Route path='cart' element={<Cart />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
