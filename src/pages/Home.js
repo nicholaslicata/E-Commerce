931686d54b253f2dc93a9c5c6c1f61a0f8a9564d
Nicholas Licata <nicholas.licata730@gmail.com>
2023-04-05 00:04:36 -0400

    initial upload

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+
+function Home() {
+    return (
+        <div className='home-container'>
+            <p className='home-p'>Welcome to my E-Commerce shop!</p>
+        </div>
+    )
+}
+
+export default Home;
