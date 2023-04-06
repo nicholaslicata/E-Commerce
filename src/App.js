821ccdde94c3f56859fb55a099150aa4b38ff163
Nicholas Licata <nicholas.licata730@gmail.com>
2023-04-06 00:05:39 -0400

    added route for page not found

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+
+// Components
 import Nav from './components/Nav';
+
+// Pages
 import Home from './pages/Home';
 import Store from './pages/Store';
 import Cart from './pages/Cart';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+
   return (
     <BrowserRouter>
       <Nav />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='store' element={<Store />} />
-          <Route path='cart' element={<Cart />} />
+          <Route path='store' element={<Store cart={cart} setCart={setCart} />} />
+          <Route path='cart' element={<Cart cart={cart} setCart={setCart} />} />
+          <Route path='*' element={<PageNotFound />} />
         </Routes>
     </BrowserRouter>
   );
