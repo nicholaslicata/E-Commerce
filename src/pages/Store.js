7ce00a8a62abeabacbbc62e02ec7099421cb3d0e
Nicholas Licata <nicholas.licata730@gmail.com>
2023-04-06 00:04:24 -0400

    initial upload

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
+import Product from '../components/Product';
 
 function Store() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getProducts();
+    });
+
+    async function getProducts() {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+    }
+
+
     return (
-        <div>
-            Store
+        <div className='product-list'>
+            {products.map(productItem => (
+                <Product productTitle={productItem.title} 
+                productImage={productItem.image} 
+                productPrice={productItem.price}/>
+            ))}
         </div>
     )
 }
